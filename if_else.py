c125e85d4c73323b7f819a861940f498a765ad3d
While_kenny <82604662+KennyKunal@users.noreply.github.com>
2025-01-31 19:25:59 +0530

    Delete if_else.py

--- if_else.py
+++
@@ -1,8 +0,0 @@
-n = int(input().strip())
-if n % 2 != 0:
-    print('Weird')
-elif n % 2 == 0:
-    if n in range(2,5) or n > 20:
-        print('Not Weird') 
-    elif n in range(6,21):
-        print('Weird')
